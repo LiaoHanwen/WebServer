027dd30c1ff181967917e4f86bdbb1b66384ed86
LiaoHanwen <login@liaohanwen.com>
2018-08-03 20:19:04 +0800

    add Edit

--- Articles/php/save_markdown.php
+++ Articles/php/save_markdown.php
@@ -21,7 +21,7 @@
 
     //rename folder
     if($OriginTitle != ""){
-        if(file_exists($dir_new)){
+        if(!file_exists($dir_delete)){
             die("error: original file does not exsit");
         }else{
             rename($dir_delete,$dir_new);
@@ -38,21 +38,20 @@
     $file_new = $dir_new."/".$ArticleTitle.".md";
     if (!file_exists($dir_new)){
         mkdir ($dir_new,0777,true);
-        $fp=fopen($file_new,"w+") or die("Unable to open file_new!");
-        fwrite($fp,$MarkDown); 
-        fclose($fp);
-        
-        //insert to mysql
-        $sql = "INSERT INTO articles (article_title, description, time)
-        VALUES ('".$ArticleTitle."', '".$Discription."', '".date("Y-m-d H:i:s")."')";
+    }
+
+    $fp=fopen($file_new,"w+") or die("Unable to open file_new!");
+    fwrite($fp,$MarkDown); 
+    fclose($fp);
     
-        if (mysqli_query($conn, $sql)) {
-            echo "save < $ArticleTitle > success";
-        } else {
-            die("insert error: " . $sql . "<br>" . mysqli_error($conn));
-        }
+    //insert to mysql
+    $sql = "INSERT INTO articles (article_title, description, time)
+    VALUES ('".$ArticleTitle."', '".$Discription."', '".date("Y-m-d H:i:s")."')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "save < $ArticleTitle > success";
     } else {
-        die("$ArticleTitle has existed");
+        die("insert error: " . $sql . "<br>" . mysqli_error($conn));
     }
 
     mysqli_close($conn);
